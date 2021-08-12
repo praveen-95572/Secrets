3c2eae50319a815ddee9a684cdb91f163031c060
praveen-95572 <praveen.95572@gmail.com>
2021-08-12 19:43:31 +0530

    Level 4 - Hashing and Salting

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+//const md5 = require("md5");
+const saltRounds = 10;
+const bcrypt = require("bcrypt");
 
 const app=express();
 
@@ -19,7 +21,7 @@ const userSchema =new mongoose.Schema({
     password:String
 });
 
-/*---------Encryption -----------   but this secret key visible easily in project location
+/*---------Encryption(lvl.2) -----------   but this secret key visible easily in project location
 // const secret ="Thisisourlittlesecret."; store in .env file
 userSchema.plugin(encrypt,{secret:process.env.SECRET , encryptedFields:["password"] });
 //-------------------------------- */
@@ -47,22 +49,25 @@ app.get("/submit", function(req , res){
 
 //-------------------------POST-------------------//
 app.post("/register",function(req,res){
-    const newUser = new User({
-        email:req.body.username,
-        password:md5(req.body.password)
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+        const newUser = new User({
+            email:req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(err)
+                console.log(err);
+            else
+                res.render("secrets");
+        });
     });
-    newUser.save(function(err){
-        if(err)
-            console.log(err);
-        else
-            res.render("secrets");
-    });
+    
 });
 
 app.post("/login",function(req,res){
     
     const username = req.body.username;
-    const password=md5(req.body.password);
+    const password=req.body.password;
     
     User.findOne({email:username},function(err,foundUser){
         if(err){
@@ -72,11 +77,13 @@ app.post("/login",function(req,res){
         else
             {
             if(foundUser){
-                if(foundUser.password == password)
-                    res.render("secrets");
-                else
+                bcrypt.compare(password,foundUser.password,function(err,result){
+                    if(result ===true)
+                        res.render("secrets");
+                    else
                     res.redirect("/login");
-                }
+                });
+            }
             else
                 res.redirect("/login");
             }
