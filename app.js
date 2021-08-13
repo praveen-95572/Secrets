accae3ccc61ee459f80fe4e409ad3605a039e700
praveen-95572 <praveen.95572@gmail.com>
2021-08-13 17:24:53 +0530

    Completion of app

--- app.js
+++ app.js
@@ -109,6 +109,15 @@ app.get("/secrets", function(req,res){
 
 });
 
+app.get("/submit",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    }
+    else{
+        res.redirect("/login");
+    }
+});
+
 app.get("/logout", function(req,res){
     req.logout();
     res.redirect("/");
